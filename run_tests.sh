b4458e9c8910045454c791dad291edaf738139ee
Dan Poirier <dan@poirier.us>
2016-01-21 08:01:33 -0500

    Canonicalize directory layout

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,5 @@ set -e
 
 find . -name '*.pyc' -delete
 flake8 opendebates
-cd opendebates
 coverage run manage.py test --keepdb "$@"
 coverage report -m --fail-under 30  # FIXME: increase minimum requirement to 80 ASAP
