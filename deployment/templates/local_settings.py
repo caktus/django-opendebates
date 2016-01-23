c8804e061fc3b00cb6ecf8ca8012bdb9bfd40ebf
Tobias McNulty <tobias@caktusgroup.com>
2016-01-22 19:17:58 -0500

    add S3 support for django-pipeline

--- deployment/templates/local_settings.py
+++ deployment/templates/local_settings.py
@@ -18,14 +18,11 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
-#STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
-#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-# Let's not use S3 quite yet
-# http://django-pipeline.readthedocs.org/en/latest/usage.html#cache-busting
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+STATICFILES_STORAGE = 'opendebates.storage.S3PipelineCachedStorage'
 
 # Auto-create the bucket if it doesn't exist
 AWS_AUTO_CREATE_BUCKET = True
