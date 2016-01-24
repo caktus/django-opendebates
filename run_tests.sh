21575586e2ff5f9497cc9fab1f27fc7cd6f66911
Vinod Kurup <vinod@kurup.com>
2016-01-24 08:23:03 -0500

    Update coverage level

--- run_tests.sh
+++ run_tests.sh
@@ -3,5 +3,6 @@ set -e
 
 find . -name '*.pyc' -delete
 flake8 .
+coverage erase
 coverage run manage.py test --keepdb "$@"
-coverage report -m --fail-under 40  # FIXME: increase minimum requirement to 80 ASAP
+coverage report -m --fail-under 70  # FIXME: increase minimum requirement to 80 ASAP
