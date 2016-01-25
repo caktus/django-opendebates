64604a2de8ce52e117d1bb2681c5536a2a805482
Dan Poirier <dan@poirier.us>
2016-01-25 08:27:37 -0500

    Remove some unnecessary settings from local_settings.py

--- deployment/templates/local_settings.py
+++ deployment/templates/local_settings.py
@@ -19,15 +19,11 @@ DEBUG = False
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
 #STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATIC_URL = '/static/'
+# (STATIC_URL is set in settings.py to /static/ which is fine without S3)
 
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
 #STATICFILES_STORAGE = 'opendebates.storage.S3PipelineCachedStorage'
-
-# Use pipeline w/cachebusting
-# https://django-pipeline.readthedocs.org/en/latest/usage.html#cache-busting
-STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 
 # Auto-create the bucket if it doesn't exist
 # AWS_AUTO_CREATE_BUCKET = True
