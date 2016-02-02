7173d3cf67bfd9a3b01f42c0832ce299c090f1d6
Dan Poirier <dan@poirier.us>
2016-02-02 08:36:11 -0500

    Fix new test to work without cache

--- opendebates/tests/test_context_processors.py
+++ opendebates/tests/test_context_processors.py
@@ -1,12 +1,12 @@
-from django.core.cache import cache
 from django.test import TestCase
+from mock import patch
 
 from opendebates.context_processors import global_vars
-from opendebates.models import NUMBER_OF_VOTES_CACHE_ENTRY
 
 
 class NumberOfVotesTest(TestCase):
     def test_number_of_votes(self):
-        cache.set(NUMBER_OF_VOTES_CACHE_ENTRY, 2)
-        context = global_vars(None)
-        self.assertEqual(2, int(context['NUMBER_OF_VOTES']))
+        with patch('opendebates.utils.cache') as mock_cache:
+            mock_cache.get.return_value = 2
+            context = global_vars(None)
+            self.assertEqual(2, int(context['NUMBER_OF_VOTES']))
