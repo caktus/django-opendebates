a246b6c96f68df9517bccf7bb1f5fc47b7f4a6a7
Dan Poirier <dan@poirier.us>
2016-02-03 07:30:14 -0500

    Use DB slave(s) on GET requests
    but send POST, PUT requests to the master
    
    I've tried registering, logging in and out,
    submitting questions, and merging, and they
    all seem to work with this enabled.

---
+++ opendebates/routing.py
@@ -0,0 +1,45 @@
+import random
+import threading
+
+from django.conf import settings
+
+
+_locals = threading.local()
+
+
+def is_thread_readwrite():
+    return getattr(_locals, 'is_readwrite', True)
+
+def set_thread_readonly_db():
+    _locals.is_readwrite = False
+
+def set_thread_readwrite_db():
+    _locals.is_readwrite = True
+
+
+class DBRouter(object):
+    def __init__(self):
+        self.read_dbs = list(settings.DATABASE_POOL.keys())
+        random.shuffle(self.read_dbs)
+        self.next_db_index = 0
+
+    def readwrite_db(self):
+        return settings.MASTER_DATABASE
+
+    def readonly_db(self):
+        # Round-robin through the read DBs
+        db = self.read_dbs[self.next_db_index]
+        self.next_db_index = (self.next_db_index + 1) % len(self.read_dbs)
+        return db
+
+    def db_for_read(self, model, **hints):
+        if is_thread_readwrite():
+            return self.readwrite_db()
+        else:
+            return self.readonly_db()
+
+    def db_for_write(self, model, **hints):
+        if is_thread_readwrite():
+            return self.readwrite_db()
+        else:
+            return self.readonly_db()
