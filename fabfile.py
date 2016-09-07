58953a0f7097201d028ef9b125c1dc64adad3e80
Tobias McNulty <tobias@caktusgroup.com>
2016-09-07 13:00:09 -0400

    add fab command to automate granting access to unaccent extension per OP-105

--- fabfile.py
+++ fabfile.py
@@ -11,3 +11,23 @@ fabulaws_logger.setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+@task
+@roles('db-master')
+def pg_create_unaccent_ext():
+    """
+    Workaround to facilitate granting the opendebates database user
+    permission to use the 'unaccent' extension in Postgres.
+    """
+    require('environment', provided_by=env.environments)
+    sudo('sudo -u postgres psql %s -c "CREATE EXTENSION IF NOT EXISTS unaccent;"'
+         '' % env.database_name)
+    for func in [
+        'unaccent(text)',
+        'unaccent(regdictionary, text)',
+        'unaccent_init(internal)',
+        'unaccent_lexize(internal, internal, internal, internal)',
+    ]:
+        sudo('sudo -u postgres psql %s -c "ALTER FUNCTION %s OWNER TO %s;"'
+             '' % (env.database_name, func, env.database_user))
