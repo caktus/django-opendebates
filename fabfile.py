52d5ccf8398a08fc013ce39b729b23c92804a102
Tobias McNulty <tobias@caktusgroup.com>
2016-09-08 18:15:24 -0400

    add presidential environment (AWS resources also manually configured at the same time)

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 import logging
 # XXX import actual commands needed
 from fabulaws.library.wsgiautoscale.api import *  # noqa
+from fabulaws.library.wsgiautoscale.api import _setup_env
 
 root_logger = logging.getLogger()
 root_logger.addHandler(logging.StreamHandler())
@@ -11,3 +12,13 @@ fabulaws_logger.setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+
+@task
+def florida(deployment_tag=env.default_deployment, answer=None):
+    _setup_env(deployment_tag, 'florida')
+
+
+@task
+def presidential(deployment_tag=env.default_deployment, answer=None):
+    _setup_env(deployment_tag, 'presidential')
