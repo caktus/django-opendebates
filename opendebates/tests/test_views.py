0611c716d41a89c8f70801df11452f33a4d11b71
Jeff Bradberry <jeff.bradberry@gmail.com>
2016-09-26 17:01:49 -0400

    Wrote tests that check for the sort pulldown entry "Top 30 Since Last Debate"
    Either the presence or absence, depending on whether there was a previous debate.

--- opendebates/tests/test_views.py
+++ opendebates/tests/test_views.py
@@ -1,8 +1,11 @@
+import datetime
 import mock
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.utils import timezone
 
+from ..models import SiteMode
 from .factories import SubmissionFactory
 
 
@@ -23,3 +26,21 @@ class ListIdeasTest(TestCase):
         """
         self.client.get(self.url)
         self.assertEqual(gsm_mock.call_count, 0)
+
+    def test_top_30_not_visible_if_no_previous_debate(self):
+        mode, _ = SiteMode.objects.get_or_create()
+        mode.previous_debate_time = None
+        mode.save()
+
+        response = self.client.get(self.url)
+        self.assertNotContains(response, "Top 30 Since Last Debate")
+
+    def test_top_30_visible_if_previous_debate(self):
+        mode, _ = SiteMode.objects.get_or_create()
+        mode.previous_debate_time = timezone.make_aware(
+            datetime.datetime(2016, 1, 1, 12)
+        )
+        mode.save()
+
+        response = self.client.get(self.url)
+        self.assertContains(response, "Top 30 Since Last Debate")
