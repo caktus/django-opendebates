c376d74631dd7f7a754025f8a06c7dcb9089e359
Jeff Bradberry <jbradberry@caktusgroup.com>
2017-03-30 16:26:22 -0400

    Raise the admin url pattern so that it doesn't get masked by the prefixed views

--- opendebates/urls.py
+++ opendebates/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^healthcheck.html$', 'opendebates.views.health_check', name='health_check'),
     url(r'^(?P<prefix>[-\w]+)/', include('opendebates.prefixed_urls')),
-    url(r'^', include('opendebates.unprefixed_urls')),
 ]
