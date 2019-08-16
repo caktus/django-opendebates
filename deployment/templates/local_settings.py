212027715be4cfce6143df12bdeb5789fe71168d
Tobias McNulty <tobias@caktusgroup.com>
2019-08-16 11:47:21 -0400

    move LOGGING configuration to settings.py

--- deployment/templates/local_settings.py
+++ deployment/templates/local_settings.py
@@ -125,52 +125,6 @@ SESSION_CACHE_ALIAS = 'session'
 
 ALLOWED_HOSTS = [{% for host in allowed_hosts %}'{{ host }}', {% endfor %}]
 
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'formatters': {
-        'basic': {
-            'format': '%(asctime)s %(name)-20s %(levelname)-8s %(message)s',
-        },
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'basic',
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-        'django.security': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-    'root': {
-        'handlers': ['console', ],
-        'level': 'INFO',
-    },
-}
-
-
 CELERYBEAT_SCHEDULE["backup-database"] = {
     "task": "opendebates.tasks.backup_database",
     "schedule": crontab(minute=0, hour="*/4"), # backup database every 4 hours
--- opendebates/settings.py
+++ opendebates/settings.py
@@ -295,3 +295,46 @@ OPTIMIZELY_KEY = None
 
 # Turn this off to never use CAPTCHA
 USE_CAPTCHA = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'basic': {
+            'format': '%(asctime)s %(name)-20s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'basic',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'django.security': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+    'root': {
+        'handlers': ['console', ],
+        'level': 'INFO',
+    },
+}
