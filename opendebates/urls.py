44c71dd58f4c3220a5a4ff40cbe7a5257667bacb
Dmitriy Chukhin <dchukhin@gmail.com>
2019-10-01 13:41:08 -0400

    use new way of including urls

--- opendebates/urls.py
+++ opendebates/urls.py
@@ -6,7 +6,7 @@ from opendebates import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='https://opendebatecoalition.com', permanent=False)),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^healthcheck.html$', views.health_check, name='health_check'),
     url(r'^(?P<prefix>[-\w]+)/', include('opendebates.prefixed_urls')),
 ]
